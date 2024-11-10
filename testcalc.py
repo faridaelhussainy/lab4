34808c203767c39010de89a7111a355d679f7511
faridaelhussainy <f.elhussainy2258@nu.edu.eg>
2024-11-10 16:06:25 +0200

    Create testcalc.py

---
+++ testcalc.py
@@ -0,0 +1,12 @@
+import unittest
+from calculator import Calculator
+class TestCalculator (unittest. TestCase):
+def test_add(self):
+# Test addition functionality
+result = Calculator-add(3, 7)
+self-assertEqual(result, 10)
+result = Calculator .add(-1, 1)
+self-assertEqual(result, ®️)
+result = Calculator .add(-1, -1)
+self.assertEqual(result, -2)
+def test_ subtract(self):
